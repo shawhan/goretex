354a437f15cc1ee229794ae56e6e82c02c205515
left <left@left.tw>
2016-10-17 01:34:57 +0800

    getjson

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
 jQuery(function($){
-	$("#photo-carasoul").owlCarousel({
-    slideSpeed : 300,
-    paginationSpeed : 400,
-    singleItem: true,
-    autoPlay: true
-  });
-
   $(".link-down").click(function(){
     $("html, body").animate({scrollTop: $(window).height()}, 500)
   });
@@ -13,7 +6,6 @@ jQuery(function($){
     event.preventDefault();
     $('#modal-iFrame').iziModal('open', this);  
   });
-
   $("#modal-iFrame").iziModal({
       title: 'The Mountain', //Modal title
       subtitle: 'A gorgeous nature time lapse.', //Modal subtitle
@@ -25,4 +17,45 @@ jQuery(function($){
       iframe: true, //In this example, this flag is mandatory. Izimodal needs to understand you will call an iFrame from here
       iframeURL: "https://www.youtube.com/embed/BmMzt02ainQ?autoPlay=1" //Link will be opened inside modal
   });
+  
+  $.getJSON("data.json", function(data) {
+      console.log(data);
+      var banner_html = "";
+      $.each(data.banner, function(k, v){
+        //TODO order by sort ASC
+        banner_html += '<div class="item"><a href="'+v.url+'" target="_blank">';
+        banner_html += '<img src="'+v.photo+'" alt="'+v.title+'"></a></div>';
+      });
+
+      $('#photo-carasoul').append(banner_html).owlCarousel({
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem: true,
+        autoPlay: true
+      });
+
+      var news_html = "";
+      $.each(data.news, function(k, v){
+        news_html += '<li><a href="'+v.url+'">'+v.title+'</a></li>';
+      });
+      $('.news-list ul').append(news_html);
+
+      var media_html = "";
+      $.each(data.media, function(k, v){
+        media_html += '<div class="post"><a href=""><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
+      });
+      $('.media-list').append(media_html);
+
+      var case_html = "";
+      $.each(data.case, function(k, v){
+        case_html += '<div class="post"><a href=""><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
+      });
+      $('.case-list').append(case_html);
+
+      var activity_html = "";
+      $.each(data.activity, function(k, v){
+        activity_html += '<div class="post"><a href=""><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
+      });
+      $('.activity-list').append(activity_html);
+    });
 });
