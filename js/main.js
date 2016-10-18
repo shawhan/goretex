7353c7a23a78083eacd099b14448ed3ff5006272
left <left@left.tw>
2016-10-19 04:10:19 +0800

    add colorbox

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 jQuery(function($){
-  $(".link-down").click(function(){
+  $('body').on('click', '.link-down', function(e){
     $("html, body").animate({scrollTop: $(window).height()}, 500)
-  });
-  $(document).on("click", ".post a", function(event){
-    event.preventDefault();
-    $('#modal-iFrame').iziModal('open', this);  
-  });
-  $("#modal-iFrame").iziModal({
-      title: 'The Mountain', //Modal title
-      subtitle: 'A gorgeous nature time lapse.', //Modal subtitle
-      fullscreen: true, //Icon to expand modal to fullscreen
-      headerColor: 'rgb(51, 76, 123)', //Color of modal header. Hexa colors allowed.
-      overlayColor: 'rgba(0, 0, 0, 0.4)', //Color of overlay behind the modal
-      iconColor: '',
-      iconClass: 'icon-chat',
-      iframe: true, //In this example, this flag is mandatory. Izimodal needs to understand you will call an iFrame from here
-      iframeURL: "https://www.youtube.com/embed/BmMzt02ainQ?autoPlay=1" //Link will be opened inside modal
+    
+  }).on("click", ".post a.colorbox", function(e){
+    e.preventDefault();
+    $.colorbox({
+      'maxWidth': '80%',
+      'maxHeight': '80%',
+      'href' : $(this).attr('href'),
+      'title' : $(this).attr('title'),
+      'onComplete': function() {
+        $("body").css({"overflow-y":"hidden"});
+      },
+      'onClosed' : function() {
+        $("body").css({"overflow-y":"visible"});
+      }
+    }); 
   });
   
   $.getJSON("data.json", function(data) {
@@ -42,19 +42,19 @@ jQuery(function($){
 
       var media_html = "";
       $.each(data.media, function(k, v){
-        media_html += '<div class="post"><a href=""><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
+        media_html += '<div class="post"><a class="colorbox" href="'+v.photo+'" title="'+v.title+'"><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
       });
       $('.media-list').append(media_html);
 
       var case_html = "";
       $.each(data.case, function(k, v){
-        case_html += '<div class="post"><a href=""><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
+        case_html += '<div class="post"><a class="colorbox" href="'+v.photo+'" title="'+v.title+'"><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
       });
       $('.case-list').append(case_html);
 
       var activity_html = "";
       $.each(data.activity, function(k, v){
-        activity_html += '<div class="post"><a href=""><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
+        activity_html += '<div class="post"><a class="colorbox" href="'+v.photo+'" title="'+v.title+'"><img src="'+v.photo+'"><span>'+v.title+'</span></a></div>';
       });
       $('.activity-list').append(activity_html);
     });
